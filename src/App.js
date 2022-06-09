9d149485464565065cb0628457f1fc0928d43ed0
Moshood <moshood.yemi10@gmail.com>
2022-06-10 06:40:08 +0800

    added branch logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import { useState } from 'react';
 
 function App() {
+  const [displayCart, setDisplayCart] = useState(false)
+
+  function showDisplayCart() {
+    setDisplayCart(true)
+  }
+
+  function hideDisplayCart() {
+    setDisplayCart(false)
+  }
   return (
     <CartProvider>
-      <Header />
+      <Header onShowCart={showDisplayCart}/>
       <main>
         <Meals />
       </main>
